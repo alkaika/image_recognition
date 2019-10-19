016cd2d6bc55e7ba50fd947fc3e67f988d665078
alkaika <52188289+alkaika@users.noreply.github.com>
2019-10-18 15:36:07 -1000

    updated heroku backend

--- src/App.js
+++ src/App.js
@@ -85,7 +85,7 @@ class App extends Component {
 
   onButtonSubmit = () => {
     this.setState({imageUrl: this.state.input});
-      fetch('https://whispering-lowlands-28773.herokuapp.com/imageurl', {
+      fetch('https://blooming-castle-68593.herokuapp.com/imageurl', {
         method: 'post',
         headers: {'Content-Type': 'application/json'},
         body: JSON.stringify({
@@ -95,7 +95,7 @@ class App extends Component {
       .then(response => response.json())
       .then(response => {
         if (response) {
-          fetch('https://whispering-lowlands-28773.herokuapp.com/image', {
+          fetch('https://blooming-castle-68593.herokuapp.com/image', {
             method: 'put',
             headers: {'Content-Type': 'application/json'},
             body: JSON.stringify({
--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -23,7 +23,7 @@ class Register extends React.Component {
   }
 
   onSubmitSignIn = () => {
-    fetch('https://whispering-lowlands-28773.herokuapp.com/register', {
+    fetch('https://blooming-castle-68593.herokuapp.com/register', {
       method: 'post',
       headers: {'Content-Type': 'application/json'},
       body: JSON.stringify({
